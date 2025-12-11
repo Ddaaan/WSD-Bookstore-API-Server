e38aa35abd6dd8e35bc503bf6922ee21bfa5aa39
Ddaaan <de2572@naver.com>
2025-12-11 14:16:33 +0900

    feat: 홈('/') 라우트 추가

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from src.app import create_app
+from flask import jsonify
 
 app = create_app("dev")
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Bookstore API 서버에 오신 것을 환영합니다!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
